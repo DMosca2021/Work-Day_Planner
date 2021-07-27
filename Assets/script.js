ba987f60367a1cc027af54ea4c4880558459c38a
dMosca <devinmosca@yahoo.com>
2021-07-26 21:59:57 -0600

    Started working on script

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+let block1 = $("#7");
+let block2 = $("#8");
+let block3 = $("#9");
+let block4 = $("#10");
+let block5 = $("#11");
+let block6 = $("#12");
+let block7 = $("#13");
+let block8 = $("#14");
+let block9 = $("#15");
+let block10 = $("#16");
+let block11 = $("#17");
+let block12 = $("#18");
+let block13 = $("#19");
+
